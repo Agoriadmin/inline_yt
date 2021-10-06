43902ed73ec861df5d21d9de74140cafe5abc157
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-10-06 07:06:48 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -27,7 +27,7 @@ else:
 ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
 
-@Client.on_message(filters.private & filters.regex(ytregex))
+@Client.on_message(filters.group & filters.private & filters.regex(ytregex))
 async def echo(bot, m: Message):
     if Config.AUTH_USERS and (m.from_user.id not in Config.AUTH_USERS):
         await m.reply_text(Presets.NOT_AUTH_TXT, reply_to_message_id=m.message_id)
